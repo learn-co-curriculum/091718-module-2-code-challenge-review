039242904c78db3f89d3e0001dec4bebd9a4df97
j-shilling <shilling.jake@gmail.com>
2018-10-22 08:29:24 -0400

    Removing ActiveRecord associations and validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,2 @@
 class Item < ApplicationRecord
-  has_many :orders
-  validates :name, presence: true, uniqueness: true
-  validates :description, presence: true, length: {minimum: 10}
-  validates :price, presence: true, numericality: {greater_than: 0}
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,2 @@
 class Order < ApplicationRecord
-  belongs_to :customer
-  belongs_to :item
-  validates :customer, presence: true
-  validates :item, presence: true
-  validates :quantity, numericality: {greater_than_or_equal_to: 1}
-  accepts_nested_attributes_for :customer, reject_if: :all_blank
-  accepts_nested_attributes_for :item, reject_if: :all_blank
 end
